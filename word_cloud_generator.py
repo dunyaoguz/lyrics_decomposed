0982daaf90d2f40156e2bc32b4029bd8dac715df
Dunya Oguz <dunyaoguz13@gmail.com>
2019-05-25 17:52:45 -0400

    create hover functionality for year buttons

--- word_cloud_generator.py
+++ word_cloud_generator.py
@@ -1,23 +1,25 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
 import pandas as pd
 import numpy as np
 import random
 
-def generate_word_cloud(df):
+def generate_word_cloud(df, year):
     '''Creates a word cloud for the given artist and saves the file as png'''
     artist = artist.replace(' ', '')
     df = pd.read_csv(f'data/{artist}.csv')
     df = df.dropna()
+    s = stopwords.words('english')
+    s.extend(['don', 'like', 'ain', 'oh', 'll', 'ooh', 'na', 'just', 've', 'tha', 'yeah', 'gon', 'gonna', 'every',
+    'wanna', 'much', 'would', 'could', 'doin', 'ever', 'uh', 'uhh', 'huh', 'yeah', 'gotta', 'bout', 'got', 'way'])
     cv = CountVectorizer(min_df=0, stop_words="english", max_features=200)
     counts = cv.fit_transform(df['lyrics'])
     words_freq = pd.DataFrame(counts.todense(), columns = cv.get_feature_names())
     words_dict = {}
-    stopwords = list(STOPWORDS)
-    stopwords.extend(['don', 'like', 'ain', 'oh', 'll', 'ooh', 'na', 'just', 've', 'tha', 'yeah'])
     for word in words_freq.columns:
-      if word not in stopwords:
+      if word not in s:
         words_dict[word] = words_freq[word].sum()
     x, y = np.ogrid[:300, :300]
     mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
@@ -35,12 +37,12 @@ def generate_word_cloud(df):
     wc.to_file(f"static/images/word_clouds/{artist}.png")
     print(f'Created word cloud for {artist}')
 
-# def word_clouds_per_year():
-#     df = pd.read_csv('sentiment_data/filtered_grand_df.csv')
-#     for year in df.release_year.unique().tolist():
-#         filtered_df = df[df.release_year == year]
-#         year_df = generate_word_cloud(filtered_df, year)
-#         print(f'Completed {year}')
+def word_clouds_per_year():
+    df = pd.read_csv('sentiment_data/filtered_grand_df.csv')
+    for year in df.release_year.unique().tolist():
+        filtered_df = df[df.release_year == year]
+        year_df = generate_word_cloud(filtered_df, year)
+        print(f'Completed {str(year)[0:4]}')
 
 if __name__ == '__main__':
     artists = pd.read_csv('data/list_of_artists.csv')
